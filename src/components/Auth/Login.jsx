e156d67ab06ba4384a3acb5a23a2fcabcc5a0744
shuganchauhan <shuganchauhan2706@gamil.com>
2026-02-23 14:47:15 +0530

    LOgin Isssued SOlved

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -31,7 +31,7 @@ const Login = ({ handleLogin }) => {
                             setEmail(e.target.value)
                         }}
                         required
-                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='Enter your email'
+                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-sm py-2 px-6 rounded-full placeholder:text-gray-400' type="email" placeholder='employee1@example.com'
                     />
                     <input
                         value={password}
@@ -39,7 +39,7 @@ const Login = ({ handleLogin }) => {
                             setPassword(e.target.value)
                         }}
                         required
-                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='Enter password' />
+                        className='outline-none bg-transparent border-2 border-emerald-600 font-medium text-lg py-2 px-6 rounded-full mt-3 placeholder:text-gray-400' type="password" placeholder='123' />
                     <button className='mt-7 text-white border-none outline-none hover:bg-emerald-700 font-semibold bg-emerald-600 text-lg py-2 px-8 w-full rounded-full placeholder:text-white'>Log in</button>
                 </form>
             </div>
